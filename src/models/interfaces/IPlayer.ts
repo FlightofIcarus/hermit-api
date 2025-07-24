2ddf2bb04e3e17331ab71b13b013dcdf6048034f
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-07-24 02:26:49 -0300

    feat: add interface to represent Player entity

---
+++ src/models/interfaces/IPlayer.ts
@@ -0,0 +1,11 @@
+interface IPlayer {
+    id: number;
+    name: string;
+    nickname: string;
+    email: string;
+    password: string;
+    matchesPlayed: number;
+    matchesWon: number;
+    matchesLost: number;
+    inGame: boolean;
+};
