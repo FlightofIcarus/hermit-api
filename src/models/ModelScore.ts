025bf0ab3df4134b1d23d372ec5c42b3d1c9d932
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-07-24 18:25:08 -0300

    feat: add getAttributes method to return Sequelize Model for Score

---
+++ src/models/ModelScore.ts
@@ -0,0 +1,40 @@
+import { DataTypes, ModelAttributeColumnOptions } from "sequelize";
+
+class Score {
+
+    private id: ModelAttributeColumnOptions = {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+    private playerId: ModelAttributeColumnOptions = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+    private matchId: ModelAttributeColumnOptions = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+    private pointsMade: ModelAttributeColumnOptions = {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+    private pointsConceded: ModelAttributeColumnOptions = {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+
+    constructor(){}
+
+    public getAttributes() {
+        return {
+            id: this.id,
+            player_id: this.playerId,
+            match_id: this.matchId,
+            points_made: this.pointsMade,
+            points_conceded: this.pointsConceded
+        };
+    }
+};
+
+export { Score };
