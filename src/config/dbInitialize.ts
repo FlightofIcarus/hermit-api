2633188a8fbc7f68d36eff4c283713aadb4158d2
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-07-27 23:31:21 -0300

    refactor: update dbInitialize to standardize db relations using pivot table and adjust model associations

--- src/config/dbInitialize.ts
+++ src/config/dbInitialize.ts
@@ -1,70 +1,36 @@
 import { DatabaseManager } from "./DatabaseManager";
 import path = require("node:path");
-import { Match } from "../models/ModelMatch";
-import { Player } from "../models/ModelPlayer";
-import { Score } from "../models/ModelScore";
+import { Match } from "../models/sequelize/ModelMatch";
+import { Player } from "../models/sequelize/ModelPlayer";
+import { Score } from "../models/sequelize/ModelScore";
+import { MatchPlayer } from "../models/sequelize/ModelMatchPlayer";
 
+const dirname = path.resolve(__dirname, "..", "db");
 
-const dbPath = path.join(__dirname, "hermitgame.db");
+const dbPath = path.join(dirname, "hermitgame.db");
     
-const dbInit = () => {
-    const mydb = new DatabaseManager({
-    dialect: "sqlite",
-    storage: dbPath,
-    });
+const mydb = new DatabaseManager({
+dialect: "sqlite",
+storage: dbPath,
+});
 
-    const MatchTable = mydb.defineModels("Match", new Match().getAttributes());
-    const PlayerTable = mydb.defineModels("Player", new Player().getAttributes());
-    const ScoreTable = mydb.defineModels("Score", new Score().getAttributes());
+const MatchTable = mydb.defineModels("Match", new Match().getAttributes());
+const PlayerTable = mydb.defineModels("Player", new Player().getAttributes());
+const ScoreTable = mydb.defineModels("Score", new Score().getAttributes());
+const MatchPlayerTable = mydb.defineModels("MatchPlayer", new MatchPlayer().getAttributes());
 
-    MatchTable.belongsTo(PlayerTable);
-    PlayerTable.hasMany(MatchTable);
-    MatchTable.hasMany(ScoreTable);
-    ScoreTable.belongsTo(MatchTable);
+PlayerTable.belongsToMany(MatchTable, { through: MatchPlayerTable, as: "matches",
+    foreignKey: "PlayerId",
+ });
+MatchTable.belongsToMany(PlayerTable, { through: MatchPlayerTable, as: "players",
+    foreignKey: "MatchId",
+ });
 
-    mydb.syncModels();
-
-    return { MatchTable, PlayerTable, ScoreTable };
-};
-
-export { dbInit };
-
-
-
+MatchTable.hasMany(ScoreTable, {
+    foreignKey: "MatchId",
+});
+PlayerTable.hasMany(ScoreTable, {
+    foreignKey: "PlayerId",});
 
 
-
-
-
-
-
-
-
-
-
-
-// match.destroy({ where: {
-//     id: 2
-// }});
-
-
-// match.create({
-//     match_name: "Match 1",
-//     status: "in_progress",
-//     started_at: new Date(),
-//     PlayerId: 1
-// });
-
-// player.create({
-//     name: "Player 1",
-//     nickname: "P1",
-//     email: "player1@example.com",
-//     password: "securepassword",
-// });
-
-// score.create({
-//     MatchId: 3,
-//     player_id: 1,
-//     points_made: 10,
-//     points_conceded: 5
-// });
+export { MatchTable, PlayerTable, ScoreTable, MatchPlayerTable, mydb };
